6c28a13836a1aff0cc7e1fcb1460a820863c364a
necelentano <necelentano@gmail.com>
2023-09-26 12:17:11 +0300

    Updated DashLayout component

--- src/components/layouts/DashLayout/index.tsx
+++ src/components/layouts/DashLayout/index.tsx
@@ -1,34 +1,56 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
 
-import { useLogOutMutation } from "../../../store/firebaseApi";
-import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Hidden from "@mui/material/Hidden";
+
+import DashboardSideNav from "../../DashboardSideNav";
+import DashboardHeader from "../../DashboardHeader";
+
+const drawerWidth = 255;
 
 const DashLayout = ({ children }: PropsWithChildren) => {
-  const [logout] = useLogOutMutation();
-  return (
-    <div>
-      <div>Navigation</div>
-      {/* Temporary  */}
-      <div
-        onClick={logout}
-        style={{
-          padding: "10px",
-          width: "200px",
-          backgroundColor: "green",
-          cursor: "pointer",
-        }}
-      >
-        LogOut
-      </div>
-      <div>
-        <Link to="tickets">Tickets</Link>
-      </div>
-      <div>
-        <Link to="settings">Settings</Link>
-      </div>
+  const [mobileOpen, setMobileOpen] = useState(false);
 
-      <div>{children}</div>
-    </div>
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  return (
+    <Box sx={{ display: "flex", minHeight: "100vh", bgcolor: "#F7F8FC" }}>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+      >
+        <Hidden smUp>
+          <DashboardSideNav
+            PaperProps={{ style: { width: drawerWidth } }}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            onCloseListItem={handleDrawerToggle}
+          />
+        </Hidden>
+        <DashboardSideNav
+          PaperProps={{ style: { width: drawerWidth } }}
+          sx={{
+            display: { sm: "block", xs: "none" },
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: 255,
+              boxSizing: "border-box",
+              bgcolor: "bgDark.main",
+              color: "#DDE2FF",
+            },
+          }}
+        />
+      </Box>
+
+      <Box sx={{ flex: "1", display: "flex", flexDirection: "column" }}>
+        <DashboardHeader onToggleSideNav={handleDrawerToggle} />
+
+        <Box sx={{ bgcolor: "#F7F8FC" }}>{children}</Box>
+      </Box>
+    </Box>
   );
 };
 
