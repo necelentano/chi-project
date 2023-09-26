23b3a1316f90433a4df816ff7873dcb260b99707
necelentano <necelentano@gmail.com>
2023-09-26 12:21:02 +0300

    Updated the Reset Password feature

--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -2,19 +2,35 @@
 
 import ResetPasswordForm from "../../components/forms/ResetPasswordForm";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import WestIcon from "@mui/icons-material/West";
+import { Link as RouterLink } from "react-router-dom";
 
-function ResetPassword() {
+const ResetPassword = () => {
   return (
     <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      height="100vh"
-      bgcolor="bgDark.main"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100%",
+        maxWidth: "100%",
+      }}
     >
+      <Box sx={{ mr: "auto", ml: "30px", mb: "20px" }}>
+        <Button
+          variant="outlined"
+          startIcon={<WestIcon />}
+          component={RouterLink}
+          to="/dashboard/settings"
+        >
+          Back to settings
+        </Button>
+      </Box>
       <ResetPasswordForm />
     </Box>
   );
-}
+};
 
 export default ResetPassword;
