5007086f8542f12e05d65cc872ab916691d7293f
necelentano <necelentano@gmail.com>
2023-10-03 13:47:31 +0300

    Added mocked data

---
+++ src/data/overviewCards.ts
@@ -0,0 +1,20 @@
+const overviewCardsData = [
+  {
+    title: "Unresolved",
+    data: 60,
+  },
+  {
+    title: "Overdue",
+    data: 16,
+  },
+  {
+    title: "Open",
+    data: 43,
+  },
+  {
+    title: "On hold",
+    data: 64,
+  },
+];
+
+export default overviewCardsData;
