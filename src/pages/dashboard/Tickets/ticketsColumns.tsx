584d1dcb8f2d15a824fa8af654b66733d59c0cd3
necelentano <necelentano@gmail.com>
2023-10-25 15:59:08 +0300

    Refactoring: Put ticketsColumns to Tickets page

---
+++ src/pages/dashboard/Tickets/ticketsColumns.tsx
@@ -0,0 +1,154 @@
+import { Avatar, Box, Chip, Typography } from "@mui/material";
+import { GridColDef } from "@mui/x-data-grid";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+import ActionsCell from "../../../components/CommonDataGrid/ActionsCell";
+import {
+  sortContactsByDate,
+  sortTicketsByPriority,
+} from "../../../utils/helpers";
+
+dayjs.extend(relativeTime);
+
+export const ticketColumns: GridColDef[] = [
+  {
+    field: "ticketDetails",
+    headerName: "Ticket details",
+    flex: 4,
+    minWidth: 500,
+    headerClassName: "datagrid-header-row",
+    renderCell: (params) => (
+      <Box sx={{ display: "flex", alignItems: "center", ml: "32px" }}>
+        <Avatar src={params.row.byUser.photo} sx={{ mr: "32px" }} />
+        <Box>
+          <Typography
+            sx={{
+              color: "#252733",
+              fontSize: "14px",
+              fontWeight: 600,
+              mb: "4px",
+            }}
+          >
+            {params.row.details}
+          </Typography>
+          <Typography
+            sx={{ color: "#C5C7CD", fontSize: "12px", fontWeight: 400 }}
+          >
+            Updated {dayjs().to(dayjs(params.row.updatedAt))}
+          </Typography>
+        </Box>
+      </Box>
+    ),
+    valueGetter: (params) => params.row.details,
+  },
+  {
+    field: "customerName",
+    headerName: "Customer name",
+    flex: 2,
+    minWidth: 200,
+    renderCell: (params) => (
+      <Box>
+        <Typography
+          sx={{
+            color: "#252733",
+            fontSize: "14px",
+            fontWeight: 600,
+            mb: "4px",
+          }}
+        >
+          {params.row.byUser.name}
+        </Typography>
+        <Typography
+          sx={{ color: "#C5C7CD", fontSize: "12px", fontWeight: 400 }}
+        >
+          on {dayjs(params.row.byUser.createdAt).format("DD.MM.YYYY")}
+        </Typography>
+      </Box>
+    ),
+    valueGetter: (params) => params.row.byUser.name,
+  },
+  {
+    field: "date",
+    headerName: "Date",
+    flex: 2,
+    minWidth: 200,
+    sortComparator: sortContactsByDate,
+    renderCell: (params) => (
+      <Box>
+        <Typography
+          sx={{
+            color: "#252733",
+            fontSize: "14px",
+            fontWeight: 600,
+            mb: "4px",
+          }}
+        >
+          {dayjs(params.row.createdAt).format("MMMM DD, YYYY")}
+        </Typography>
+        <Typography
+          sx={{ color: "#C5C7CD", fontSize: "12px", fontWeight: 400 }}
+        >
+          {dayjs(params.row.createdAt).format("h:mm A")}
+        </Typography>
+      </Box>
+    ),
+    valueGetter: (params) => dayjs(params.row.createdAt).format("MMMM D, YYYY"),
+  },
+  {
+    field: "priority",
+    headerName: "Priority",
+    flex: 2,
+    minWidth: 200,
+    sortComparator: sortTicketsByPriority,
+    renderCell: (params) => {
+      const { priority } = params.row;
+
+      let bgcolor, text;
+      switch (priority) {
+        case "low":
+          bgcolor = "#FEC400";
+          text = "low";
+          break;
+        case "normal":
+          bgcolor = "#29CC97";
+          text = "normal";
+          break;
+        case "high":
+          bgcolor = "#F12B2C";
+          text = "high";
+          break;
+        default:
+          bgcolor = "#29CC97";
+          text = "normal";
+      }
+      return (
+        <Box>
+          <Chip
+            label={text}
+            sx={{
+              bgcolor,
+              textTransform: "uppercase",
+              fontSize: "11px",
+              fontWeight: 700,
+              color: "#FFFFFF",
+              px: "6px",
+              py: "4px",
+            }}
+          />
+        </Box>
+      );
+    },
+    valueGetter: (params) => params.row.priority,
+  },
+  {
+    field: "actions",
+    headerName: "",
+    sortable: false,
+    filterable: false,
+    minWidth: 50,
+    renderCell: (params) => {
+      return <ActionsCell row={params.row} />;
+    },
+  },
+];
