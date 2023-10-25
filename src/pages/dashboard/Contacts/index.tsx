117ca72e1edc1d7f46488a5bec8869b7321394cc
necelentano <necelentano@gmail.com>
2023-10-25 16:13:49 +0300

    Used a CommonDataGrid in the Contacts page component

--- src/pages/dashboard/Contacts/index.tsx
+++ src/pages/dashboard/Contacts/index.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
 import { Container, useMediaQuery } from "@mui/material";
-import ContactsDataGrid from "../../../components/ContactsDataGrid";
+import CommonDataGrid from "../../../components/CommonDataGrid";
+import { TAuthError, TSortModel } from "../../../utils/types";
+import { contactColumns } from "./contactsColumns";
+import { contactsData } from "../../../data/contactsData";
+import { addContactFormData } from "../../../components/forms/formsData/addContact";
 
 function Contacts() {
   const isMobile = useMediaQuery("(max-width:600px)");
   const isTablet = useMediaQuery("(max-width:900px)");
+
+  // Specify sortModel
+  const sortModel: TSortModel[] = [
+    {
+      field: "createdAt",
+      sort: "desc",
+    },
+  ];
+
+  // RTK Query hook here with our contacts (data, isLoading, isError, error, etc.) - not implemented because there is no backend yet
+  // At this moment we are using mocked data = ticketsData
+  //Here we do a loading state for fake request
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(true), 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  // Here we also use RTK Query hook to handle form, not implemented because there is no backend yet
 
   return (
     <Container
@@ -19,7 +47,18 @@ function Contacts() {
         },
       ]}
     >
-      <ContactsDataGrid />
+      <CommonDataGrid
+        tableColumns={contactColumns}
+        tableData={contactsData}
+        tableSortModel={sortModel}
+        tableIsLoading={!loading}
+        formTemplate={addContactFormData}
+        formSubmitHandler={() => console.log("POST: create new contact")}
+        formIsError={false}
+        formError={{} as TAuthError}
+        formIsLoading={false}
+        formButtonText="Add contact"
+      />
     </Container>
   );
 }
