339b887bf6309cf70e43642bfaaa9842acd31e4d
necelentano <necelentano@gmail.com>
2023-11-07 12:54:52 +0200

    Remove getHeaderTitle from hooks; added getHeaderTitle helper function

--- src/utils/helpers/index.ts
+++ src/utils/helpers/index.ts
@@ -35,3 +35,15 @@ export const shortImageName = (imgName: string, length: number) => {
 
   return imgName;
 };
+
+export const getHeaderTitle = (pathName: string): string => {
+  const arr = pathName.split("/");
+
+  const pathNameArr = arr[arr.length - 1].split("-");
+
+  return pathNameArr
+    .map((s) => {
+      return s.charAt(0).toUpperCase() + s.slice(1);
+    })
+    .join(" ");
+};
